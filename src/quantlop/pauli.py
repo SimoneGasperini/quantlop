ae10b2fbc660be1e50e92bfdd742396f8257eb7f
SimoneGasperini <simone.gasperini4@unibo.it>
2025-12-12 08:21:46 +0100

    Implement class for Pauli word linear operator

--- src/quantlop/pauli.py
+++ src/quantlop/pauli.py
@@ -1,37 +1,32 @@
 import numpy as np
-from pylops import LinearOperator
+from scipy.sparse.linalg import LinearOperator
 
 
-class Identity(LinearOperator):
-    def __init__(self):
-        super().__init__(shape=(2, 2), dtype=complex)
+class PauliWord(LinearOperator):
+    def __init__(self, string):
+        n = len(string)
+        super().__init__(shape=(2**n, 2**n), dtype=complex)
+        self._string = string
 
-    def _matvec(self, psi):
-        return np.array(psi)
-
-
-class PauliX(LinearOperator):
-    def __init__(self):
-        super().__init__(shape=(2, 2), dtype=complex)
-
-    def _matvec(self, psi):
-        alpha, beta = psi
-        return np.array([beta, alpha])
-
-
-class PauliY(LinearOperator):
-    def __init__(self):
-        super().__init__(shape=(2, 2), dtype=complex)
-
-    def _matvec(self, psi):
-        alpha, beta = psi
-        return np.array([-1j * beta, 1j * alpha])
-
-
-class PauliZ(LinearOperator):
-    def __init__(self):
-        super().__init__(shape=(2, 2), dtype=complex)
-
-    def _matvec(self, psi):
-        alpha, beta = psi
-        return np.array([alpha, -beta])
+    def _matvec(self, vec):
+        n = len(self._string)
+        vec_new = np.empty(2**n, dtype=complex)
+        for i in range(len(vec)):
+            b = [(i >> k) & 1 for k in reversed(range(n))]
+            b_new = b.copy()
+            phase = 1.0 + 0j
+            for qubit, pauli in enumerate(self._string):
+                if pauli == "X":
+                    b_new[qubit] ^= 1
+                elif pauli == "Y":
+                    b_new[qubit] ^= 1
+                    if b[qubit] == 0:
+                        phase *= 1j
+                    else:
+                        phase *= -1j
+                elif pauli == "Z":
+                    if b[qubit] == 1:
+                        phase *= -1
+            j = sum(b << k for k, b in enumerate(reversed(b_new)))
+            vec_new[j] = phase * vec[i]
+        return vec_new
