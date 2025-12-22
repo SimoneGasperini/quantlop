040a2d08f6de8d302ee732fb368c71e7dd217b34
SimoneGasperini <simone.gasperini4@unibo.it>
2025-12-22 13:29:25 +0100

    Working on PauliWord._matvec optimization

--- src/quantlop/pauliword.py
+++ src/quantlop/pauliword.py
@@ -1,5 +1,18 @@
+from functools import cache
 import numpy as np
 from scipy.sparse.linalg import LinearOperator
+
+
+@cache
+def _yz_phase(nq):
+    y_phase = []
+    z_phase = []
+    for axis in range(nq):
+        shape = [1] * nq
+        shape[axis] = 2
+        y_phase.append(np.array([-1j, 1j], dtype=complex).reshape(shape))
+        z_phase.append(np.array([1, -1], dtype=complex).reshape(shape))
+    return y_phase, z_phase
 
 
 class PauliWord(LinearOperator):
@@ -32,16 +45,13 @@ class PauliWord(LinearOperator):
 
     def _matvec(self, vec):
         out = vec.reshape((2,) * self.num_qubits)
+        y_phase, z_phase = _yz_phase(nq=self.num_qubits)
         for axis, pauli in enumerate(self.string):
             if pauli == "X":
                 out = np.flip(out, axis=axis)
             elif pauli == "Y":
                 out = np.flip(out, axis=axis)
-                shape = [1] * self.num_qubits
-                shape[axis] = 2
-                out = -1j * out * np.array([1, -1], dtype=complex).reshape(shape)
+                out = out * y_phase[axis]
             elif pauli == "Z":
-                shape = [1] * self.num_qubits
-                shape[axis] = 2
-                out = out * np.array([1, -1], dtype=complex).reshape(shape)
+                out = out * z_phase[axis]
         return self.coeff * out.reshape(-1)
