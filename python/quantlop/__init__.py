dafac01341bf33a63204ce39ff9625ed11c731c1
SimoneGasperini <simone.gasperini4@unibo.it>
2026-07-28 09:50:32 +0200

    Move utils module into Python package

--- python/quantlop/__init__.py
+++ python/quantlop/__init__.py
@@ -1,5 +1,6 @@
 from .pauliword import PauliWord
 from .hamiltonian import Hamiltonian
 from .evolution import evolve_higham, evolve_krylov
+from . import utils
 
-__all__ = ["Hamiltonian", "PauliWord", "evolve_higham", "evolve_krylov"]
+__all__ = ["Hamiltonian", "PauliWord", "evolve_higham", "evolve_krylov", "utils"]
--- tests/test_evolution.py
+++ tests/test_evolution.py
@@ -3,17 +3,15 @@ import numpy as np
 import scipy as sp
 import quantlop as ql
 
-from utils import get_rand_statevector, get_rand_hamiltonian
-
 EVOLVE_FUNCS = [ql.evolve_higham, ql.evolve_krylov]
 
 
 @pytest.mark.parametrize("evolve_func", EVOLVE_FUNCS)
 @pytest.mark.parametrize("num_qubits", range(1, 11))
 def test_against_scipy(evolve_func, num_qubits):
-    psi = get_rand_statevector(num_qubits)
+    psi = ql.utils.get_rand_statevector(num_qubits)
     num_terms = num_qubits * 5
-    ham = get_rand_hamiltonian(num_qubits, num_terms=num_terms)
+    ham = ql.utils.get_rand_hamiltonian(num_qubits, num_terms=num_terms)
     psi_scipy = sp.linalg.expm(-1j * ham.matrix()) @ psi
     psi_quantlop = evolve_func(ham, psi)
     assert np.allclose(psi_scipy, psi_quantlop)
@@ -22,8 +20,8 @@ def test_against_scipy(evolve_func, num_qubits):
 @pytest.mark.parametrize("evolve_func", EVOLVE_FUNCS)
 @pytest.mark.parametrize("num_qubits", range(1, 6))
 def test_identity_evolution(evolve_func, num_qubits):
-    psi = get_rand_statevector(num_qubits)
+    psi = ql.utils.get_rand_statevector(num_qubits)
     num_terms = num_qubits * 5
-    ham = get_rand_hamiltonian(num_qubits, num_terms=num_terms)
+    ham = ql.utils.get_rand_hamiltonian(num_qubits, num_terms=num_terms)
     new_psi = evolve_func(ham, psi, theta=0.0)
     assert np.allclose(psi, new_psi)
