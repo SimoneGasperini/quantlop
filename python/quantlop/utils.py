1e20f049c9f8c658466a9c667a69246482fefad6
SimoneGasperini <simone.gasperini4@unibo.it>
2026-07-29 10:26:47 +0200

    Fix random objects generation in utils module

--- python/quantlop/utils.py
+++ python/quantlop/utils.py
@@ -4,21 +4,23 @@ import quantlop as ql
 chars = ("I", "X", "Y", "Z")
 
 
-def get_rand_statevector(num_qubits, seed=None):
-    rng = np.random.default_rng(seed=seed)
+def get_rand_statevector(num_qubits, rng=None):
+    if rng is None:
+        rng = np.random.default_rng()
     real = rng.random(2**num_qubits)
     imag = rng.random(2**num_qubits)
     psi = real + 1j * imag
     return psi / np.linalg.norm(psi)
 
 
-def get_rand_pauliword(num_qubits, seed=None):
-    rng = np.random.default_rng(seed=seed)
+def get_rand_pauliword(num_qubits, rng=None):
+    if rng is None:
+        rng = np.random.default_rng()
     coeff = rng.random()
     string = "".join(rng.choice(chars, size=num_qubits))
     return ql.PauliWord(coeff=coeff, string=string)
 
 
-def get_rand_hamiltonian(num_qubits, num_terms, seed=None):
-    pwords = [get_rand_pauliword(num_qubits, seed=seed) for _ in range(num_terms)]
+def get_rand_hamiltonian(num_qubits, num_terms, rng=None):
+    pwords = [get_rand_pauliword(num_qubits, rng=rng) for _ in range(num_terms)]
     return ql.Hamiltonian(pwords=pwords)
